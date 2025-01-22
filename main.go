b4b2fe378d7f54f2c8cfccf3e5bc706c33b69bcc
migomi3 <migomi3@gmail.com>
2025-01-21 21:09:00 -0500

    added healthz endpoint

--- main.go
+++ main.go
@@ -12,7 +12,13 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
 
 	err := server.ListenAndServe()
 	if err != nil {
