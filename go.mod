c5fbbd293948b8a8366da036375e1dd5542bc916
migomi3 <migomi3@gmail.com>
2025-01-29 00:56:25 -0500

    added backend architecture with postgres and goose

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/migomi3
 
 go 1.23.2
 
-require golang.org/x/net v0.34.0
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/migomi3/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	mux := http.NewServeMux()
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	cfg := apiConfig{}
+	cfg := apiConfig{
+		db: database.New(db),
+	}
 	cfg.fileserverHits.Store(0)
 
 	mux.HandleFunc("POST /api/validate_chirp", cfg.validateChirpHandler)
@@ -26,7 +42,7 @@ func main() {
 	mux.HandleFunc("GET /admin/healthz", healthEndpointHandler)
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("./")))))
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatalln(err)
 	}
